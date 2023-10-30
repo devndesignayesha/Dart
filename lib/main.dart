7e9b0694aa74d90df7d5cfce61dd6ae75737893a
Ayesha Amjad <ashamjad007@gmail.com>
2023-10-30 12:21:56 +0500

    part 7 of dart tutorials practice

--- lib/main.dart
+++ lib/main.dart
@@ -6,4 +6,19 @@ void main() {
 
   sum = number + numberTwo;
   print(sum);
+  /*Print your all family name in ascending order 
+Create square shape with steric(*) 
+Create rectangle shape with steric(*)
+Create triangle shapes with  steric(*)
+Create pentagon shape with steric(*)
+Create circle shape with  steric(*)
+Create Heart shape with steric (*)
+Program to print full pyramid using *
+*/
+//family names
+  print("My Family names");
+  print("Mr Husband");
+  print("Mr Wifey");
+
+  //
 }

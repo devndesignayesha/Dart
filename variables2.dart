008951b05dae5c4f7bb119bbec16c871416c8fdb
Ayesha Amjad <ashamjad007@gmail.com>
2023-11-01 17:07:29 +0500

    Create variables2.dart

---
+++ variables2.dart
@@ -0,0 +1,21 @@
+void main() {
+  String firstname;
+  //double quotes
+  firstname = "Dev N ";
+  print("Frist name: $firstname");
+
+  String lastname;
+  //double quotes
+  lastname = "Design";
+  print("Last name: $lastname");
+
+  String gender;
+  //double quotes
+  gender = "Female";
+  print("Gender: $gender");
+
+  String profession;
+  //double quotes
+  profession = "Developer";
+  print("Profession: $profession");
+}

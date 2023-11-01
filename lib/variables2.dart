391632c97ea72ae0a0dead09471da68723d3ff24
Ayesha Amjad <ashamjad007@gmail.com>
2023-11-01 17:55:04 +0500

    Update variables2.dart

--- lib/variables2.dart
+++ lib/variables2.dart
@@ -18,4 +18,26 @@ void main() {
   //double quotes
   profession = "Developer";
   print("Profession: $profession");
+
+  //understanding boolean variables
+  bool islearning = true;
+  print("Is this user learning: $islearning");
+
+  bool isdoctor = false;
+  print("Is this user a Dcotor: $isdoctor");
+
+  //understanding list
+
+  List<int> age = [12, 23, 38, 21, 43];
+  print("This is the Age List: $age");
+
+  //accessing elements from the list by their index number
+
+  print("Element at 2:");
+  print(age.elementAt(2));
+
+  print("Element at 3:");
+  print(age.elementAt(3));
+
+  print(age[0]);
 }
